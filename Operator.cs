8e3b2776e59bee634504f4f727df5ecd74860bcb
Dyl777 <117218643+Dyl777@users.noreply.github.com>
2025-01-14 15:51:16 +0100

    Update Operator.cs

--- Operator.cs
+++ Operator.cs
@@ -1,1 +1,14 @@
 
+class Operator
+{
+    private static readonly string[] validOperators = { "+", "-", "*", "/", "%" };
+
+    public static void ValidateOperator(string op)
+    {
+        if (Array.IndexOf(validOperators, op) == -1)
+        {
+            Console.WriteLine("Invalid operator");
+            Environment.Exit(1);
+        }
+    }
+}
