e90bfddab7302ef27f84a67d45bae7d80f46a3a4
Dyl777 <117218643+Dyl777@users.noreply.github.com>
2025-01-14 16:10:58 +0100

    Update DoubleOperation.cs

--- DoubleOperation.cs
+++ DoubleOperation.cs
@@ -1,1 +1,13 @@
-
+class DoubleOperations : Operations<double>
+{
+    public override double Add(double a, double b) => a + b;
+    public override double Subtract(double a, double b) => a - b;
+    public override double Multiply(double a, double b) => a * b;
+    public override double Divide(double a, double b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException("Division by zero is not allowed");
+        return a / b;
+    }
+    public override double Modulo(double a, double b) => a % b;
+}
