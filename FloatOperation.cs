8146c5a23f7813541461986ae00c504990d05582
Dyl777 <117218643+Dyl777@users.noreply.github.com>
2025-01-14 16:17:12 +0100

    Update FloatOperation.cs

--- FloatOperation.cs
+++ FloatOperation.cs
@@ -1,1 +1,13 @@
-
+class FloatOperations : Operations<float>
+{
+    public override float Add(float a, float b) => a + b;
+    public override float Subtract(float a, float b) => a - b;
+    public override float Multiply(float a, float b) => a * b;
+    public override float Divide(float a, float b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException("Division by zero is not allowed");
+        return a / b;
+    }
+    public override float Modulo(float a, float b) => a % b;
+}
