a22c78f47d710f1c02fd4056980e201440466c3a
Dyl777 <117218643+Dyl777@users.noreply.github.com>
2025-01-14 16:05:23 +0100

    Create IntOperation.cs

---
+++ IntOperation.cs
@@ -0,0 +1,13 @@
+class IntOperations : Operations<int>
+{
+    public override int Add(int a, int b) => a + b;
+    public override int Subtract(int a, int b) => a - b;
+    public override int Multiply(int a, int b) => a * b;
+    public override int Divide(int a, int b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException("Division by zero is not allowed");
+        return a / b;
+    }
+    public override int Modulo(int a, int b) => a % b;
+}
