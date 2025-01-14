b107a08ba3a7ce555ae5bcc8d9ae2285545a9b78
Dyl777 <117218643+Dyl777@users.noreply.github.com>
2025-01-14 15:38:47 +0100

    Update Operator.cs

--- Operator.cs
+++ Operator.cs
@@ -1,1 +1,26 @@
+abstract class Operations<T>
+{
+    public abstract T Add(T a, T b);
+    public abstract T Subtract(T a, T b);
+    public abstract T Multiply(T a, T b);
+    public abstract T Divide(T a, T b);
+    public abstract T Modulo(T a, T b);
+
+    public static T PerformOperation(string op, T a, T b)
+    {
+        var instance = (Operations<T>)Activator.CreateInstance(typeof(T).Name == "Int32" ? typeof(IntOperations) :
+                            typeof(T).Name == "Single" ? typeof(FloatOperations) :
+                            typeof(DoubleOperations));
+
+        return op switch
+        {
+            "+" => instance.Add(a, b),
+            "-" => instance.Subtract(a, b),
+            "*" => instance.Multiply(a, b),
+            "/" => instance.Divide(a, b),
+            "%" => instance.Modulo(a, b),
+            _ => throw new ArgumentException("Unsupported operator")
+        };
+    }
+}
 
